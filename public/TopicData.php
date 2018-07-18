15e4c5deb95cedd0f1624542367b8ec33244b53a
quik <iagopd@gmail.com>
2018-07-18 11:01:30 +0200

    edit

--- public/TopicData.php
+++ public/TopicData.php
@@ -35,9 +35,40 @@ class TopicData {
         $query->execute($data);
     }
 
+    public function getTopic($id)
+    {
+        $sql = "SELECT * FROM topics WHERE id = :id LIMIT 1";
+        $query = $this->connection->prepare($sql);
+
+        $values = [':id' => $id];
+        $query->execute($values);
+
+        return $query->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($data)
+    {
+        $query = $this->connection->prepare(
+            "UPDATE topics 
+                SET 
+                    title = :title, 
+                    description = :description
+                WHERE
+                    id = :id"
+        );
+
+        $data = [
+            ':id' => $data['id'],
+            ':title' => $data['title'],
+            ':description' => $data['description']
+        ];
+
+        return $query->execute($data);
+    }
+
     public function __construct()
-{
-    $this->connect();
-}
+	{
+    	$this->connect();
+	}
 }
 ?>
